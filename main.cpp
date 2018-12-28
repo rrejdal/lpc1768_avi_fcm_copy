b3872853cf832a3c674ae50c120ed36a74fd5603
steve prince <sprince@avidroneaerospace.com>
2018-12-28 12:41:21 -0500

    Manually sort out issues with merge from 210_TL_RC1.1. Branch now reflects changes from 210_TL_RC1.1 & RC1.2

--- main.cpp
+++ main.cpp
@@ -53,8 +53,11 @@ void HeadingUpdate(float heading_rate, float dT);
 extern int LoadConfiguration(const ConfigData **pConfig);
 extern int LoadCompassCalibration(const CompassCalibrationData **pCompass_cal);
 extern int SaveNewConfig(void);
-extern int SavePIDUpdates(FlightControlData *fcm_data);
+extern int UpdateFlashConfig(FlightControlData *fcm_data);
 extern int SaveCompassCalibration(const CompassCalibrationData *pCompass_cal);
+
+extern int EraseFlash(void);
+extern int SetJtag(int state);
 
 void CompassCalDone(void);
 
@@ -2015,9 +2018,7 @@ static void ProcessFlightMode(FlightControlData *hfc)
                 hfc->touchdown_time = hfc->time_ms;
 
                 // TODO::SP: Error Handling on Flash write error??
-                if (hfc->pid_params_changed) {
-                	SavePIDUpdates(hfc);
-                }
+                UpdateFlashConfig(hfc);
             }
         }
         else
@@ -2191,7 +2192,12 @@ static void ServoUpdateRAW(float dT)
         if (pConfig->power_node) {
             WriteToPowerNodeServos();
         }
-        else if (pConfig->fcm_servo) {
+
+        if (pConfig->can_servo) {
+            WriteToServoNodeServos(pConfig->num_servo_nodes);
+        }
+
+        if (pConfig->fcm_servo) {
             WriteToFcmServos();
         }
     }
@@ -2966,7 +2972,12 @@ static void ServoUpdate(float dT)
         if (pConfig->power_node) {
             WriteToPowerNodeServos();
         }
-        else {
+
+        if (pConfig->can_servo) {
+            WriteToServoNodeServos(pConfig->num_servo_nodes);
+        }
+
+        if (pConfig->fcm_servo) {
             WriteToFcmServos();
         }
     }
@@ -3124,8 +3135,10 @@ static void UpdateBatteryStatus(float dT)
 
     p->power_lp = LP_RC(power, p->power_lp, 0.5f, dT);
 
-    /* when current is below 0.2C, battery is considered unloaded */
-    if (I < (0.0002f * hfc.rw_cfg.battery_capacity)) {
+    // TODO::SP: This needs to be tested and made a configurable value
+    /* when current is below 20A (0.2A normally), battery is considered unloaded */
+    //if (I < (0.0002f * hfc.rw_cfg.battery_capacity)) {
+    if (I < (0.02f * hfc.rw_cfg.battery_capacity)) {
 
         float level = UnloadedBatteryLevel(p->Vmain / Max(1, pConfig->battery_cells), pConfig->V2Energy);
         float Ecurr = p->energy_total * level;
@@ -3911,6 +3924,25 @@ void do_control()
         }
     }
 
+    // As ground speed increases, trust the gps heading more
+    if(     (hfc.gps_speed >= pConfig->gps_speed_heading_threshold)
+         && (gps.gps_data_.PDOP*0.01f < 2.00f )
+         && (hfc.full_auto || (hfc.control_mode[PITCH] >= CTRL_MODE_SPEED) )
+         && (ABS(hfc.ctrl_out[RATE][YAW]) <= pConfig->yaw_heading_threshold) ) {
+
+        float current_offset = hfc.gps_heading - hfc.heading;
+        wrap180(&current_offset);
+
+        if ( ABS(current_offset) >= pConfig->heading_offset_threshold)   {
+            hfc.heading_offset = hfc.gps_heading - hfc.compass_heading_lp;
+            wrap180(&hfc.heading_offset);
+        }
+    }
+
+    hfc.heading = hfc.compass_heading_lp + hfc.heading_offset;
+    wrap180(&hfc.heading);
+
+
     if (pConfig->baro_enable == 1) {
     	hfc.baro_dT += dT;
     	baro_altitude_raw_prev = hfc.baro_altitude_raw_lp;
@@ -3961,7 +3993,7 @@ void do_control()
         /* orient is in rad, gyro in deg */
         hfc.gyroOfs[PITCH] = -PID(&hfc.pid_IMU[PITCH], hfc.SmoothAcc[PITCH]*R2D-hfc.bankPitch,hfc.IMUorient[PITCH]*R2D, dT);
         hfc.gyroOfs[ROLL]  = -PID(&hfc.pid_IMU[ROLL],  hfc.SmoothAcc[ROLL]*R2D+hfc.bankRoll,  hfc.IMUorient[ROLL]*R2D,  dT);
-        hfc.gyroOfs[YAW]   = -PID(&hfc.pid_IMU[YAW],   hfc.compass_heading_lp,                hfc.IMUorient[YAW]*R2D,   dT);
+        hfc.gyroOfs[YAW]   = -PID(&hfc.pid_IMU[YAW],   hfc.heading,                hfc.IMUorient[YAW]*R2D,   dT);
         /*
         if (!(hfc.print_counter & 0x3ff)) {
             debug_print("Gyro %+6.3f %+6.3f %+6.3f  IMU %+6.3f %+6.3f %+6.3f  Err %+6.3f %+6.3f %+6.3f\r\n",
@@ -4549,6 +4581,36 @@ static void ProcessUserCmnds(char c)
 
         usb_print("TYPE[IMU], ID[%d], YEAR[%d], VARIANT[%d]\r\n", mpu.eeprom->id_num, mpu.eeprom->board_year, mpu.eeprom->board_type);
     }
+    else if (c == 'D') {
+        serial.scanf("%19s", request);
+        if (strcmp(request, "unlock") == 0) {
+            if (SetJtag(UNLOCK_JTAG) == 0) {
+                usb_print("ACK");
+            }
+            else {
+                usb_print("NACK");
+            }
+        }
+        else if (strcmp(request, "lock") == 0) {
+            if (SetJtag(LOCK_JTAG) == 0) {
+                usb_print("ACK");
+            }
+            else {
+                usb_print("NACK");
+            }
+        }
+        else if (strcmp(request, "eraseall") == 0) {
+            if (EraseFlash() == 0) {
+                usb_print("ACK");
+            }
+            else {
+                usb_print("NACK");
+            }
+        }
+        else {
+            usb_print("INVALID");
+        }
+    }
 }
 
 void ProcessButtonSelection()
@@ -5088,6 +5150,8 @@ void InitializeRuntimeData(void)
     }
 
     hfc.box_dropper_ = 0;
+
+    hfc.heading_offset = 0; //pConfig->heading_offset;
 }
 
 /**
@@ -5144,6 +5208,10 @@ static int InitCanbusNodes(void)
   */
 int main()
 {
+
+#if defined (CRP_LOCK)
+    SetJtag(LOCK_JTAG);
+#endif
 
     led1 = 1;
 
