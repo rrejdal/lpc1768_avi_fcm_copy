f08ab35db9aac01ac193e0cf51d8e0419b01351d
Mark Ibrahim <mibrahim@avidroneaerospace.com>
2019-04-16 17:44:41 -0400

    Fix from last pull request.

--- main.cpp
+++ main.cpp
@@ -5536,7 +5536,7 @@ void InitializeRuntimeData(void)
     LP4_Init(&hfc.lp_baro4, pConfig->baro_lp_freq);
     LP4_Init(&hfc.lp_baro_vspeed4, pConfig->baro_vspeed_lp_freq);
 
-    //hfc.Pos_GPS_IMU_Blend = pConfig->Pos_GPS_IMU_BlendReg;
+    hfc.Pos_GPS_IMU_Blend = pConfig->Pos_GPS_IMU_BlendReg;
     hfc.telem_ctrl_period = Max(hfc.telem_ctrl_period, (pConfig->telem_min_ctrl_period * 1000));
 
     hfc.throttle_value   = -pConfig->Stick100range;
